f8b01cfe7dfab02febd8c576a5f9723f2ddfb151
NickLiffen <nickliffen@gmail.com>
2021-10-29 11:52:38 +0100

    Fixed GHAS Problem

--- src/utils/enableGHAS.ts
+++ src/utils/enableGHAS.ts
@@ -5,61 +5,20 @@ import { Octokit } from "./octokitTypes";
 import {
   updateReposResponse,
   updateReposParameters,
-  listCodeScanningParameters,
-  listCodeScanningResponse,
 } from "./octokitTypes";
-
-const checkCodeScanningStatus = async (
-  requestParams: listCodeScanningParameters,
-  octokit: Octokit
-): Promise<boolean> => {
-  try {
-    const { status, data } = (await octokit.request(
-      "GET /repos/{owner}/{repo}/code-scanning/analyses",
-      requestParams
-    )) as listCodeScanningResponse;
-
-    const enabled =
-      status === 200 && data.length > 0 ? true : (false as boolean);
-    return enabled as boolean;
-  } catch (err) {
-    error(
-      `Problem checking if GHAS is enabled on the following repository: ${requestParams.repo}. The error was: ${err}`
-    );
-    throw err;
-  }
-};
 
 export const enableGHAS = async (
   repo: string,
   octokit: Octokit
 ): Promise<response> => {
-  const requestParamsCheckCodeScanning = {
+  const requestParamsEnableCodeScanning = {
     owner,
     repo,
     mediaType: {
       previews: ["dorian"],
     },
-  } as listCodeScanningParameters;
-
-  const requestParamsEnableCodeScanning = {
-    ...requestParamsCheckCodeScanning,
     security_and_analysis: { advanced_security: { status: "enabled" } },
   } as updateReposParameters;
-
-  const enabled = (await checkCodeScanningStatus(
-    requestParamsCheckCodeScanning,
-    octokit
-  )) as boolean;
-
-  inform(`Is GHAS enabled already for ${repo}? : ${enabled}`);
-
-  if (enabled === true) {
-    return {
-      status: 200,
-      message: "Repository already had GHAS Enabled",
-    };
-  }
 
   try {
     const { status } = (await octokit.request(
