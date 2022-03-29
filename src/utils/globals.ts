5e8e5bb85e8809687599c43ed68b41db19415302
Alan Gonzalez <alandgonzalez@gmail.com>
2022-03-29 10:06:11 -0400

    Update globals.ts

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -32,3 +32,7 @@ export const user = process.cwd().split("/")[2] as string;
 export const winUser = process.cwd().split("\\")[2] as string;
 export const reposFileLocation = "./bin/repos.json" as string;
 export const orgsFileLocation = "./bin/organizations.json" as string;
+export const dryRun = process.env.DRY_RUN  === "true" ? true : false;
+export const useRegex = process.env.REPO_ENABLE_REGEX  === "true" ? true : false;
+export const repoIncludeRegex = process.env.REPO_INCLUDE_REGEX || ("" as string);
+export const repoExcludeRegex = process.env.REPO_EXCLUDE_REGEX || ("" as string);
