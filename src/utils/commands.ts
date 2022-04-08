9cfd8b2fe3c825f9145427aaf1b4a9954662a20a
Alan Gonzalez <alandgonzalez@gmail.com>
2022-04-08 13:40:22 -0400

    Git Clone speedup on mac - commands.ts (#46)
    * Update commands.ts
    
    * Pretty js
    
    Co-authored-by: Alan Gonzalez <93621282+agonzalez-plume@users.noreply.github.com>

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -95,7 +95,14 @@ export const macCommands = (
     },
     {
       command: "git",
-      args: ["clone", `${baseURL}/${owner}/${repo}.git`],
+      args: [
+        "clone",
+        "--depth",
+        "1",
+        "--filter=blob:none",
+        "--sparse",
+        `${baseURL}/${owner}/${repo}.git`,
+      ],
       cwd: `/Users/${user}/${destDir}/${tempDIR}`,
     },
     {
