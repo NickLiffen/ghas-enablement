52a32342c3fa49cfd7d8bed0b91422200e424e0d
Jors Matthys <jors.matthys@liantis.be>
2022-07-12 13:34:10 +0200

    Fix CodeQL analysis check.

--- src/utils/checkCodeQLEnablement.ts
+++ src/utils/checkCodeQLEnablement.ts
@@ -21,8 +21,9 @@ export const checkIfCodeQLHasAlreadyRanOnRepo = async (
       "GET /repos/{owner}/{repo}/code-scanning/analyses",
       requestParams
     )) as checkCodeScanningAnalysesResponse;
-    if (data.length === 0) return false;
-    return true;
+    // If there are no analysis, the result is not a list and data.length will return undefined.
+    if (data.length > 0) return true;
+    return false;
   } catch (e) {
     if (e instanceof RequestError) {
       if (e.status == 404) return false; // 404 result means no codeQL scans found
